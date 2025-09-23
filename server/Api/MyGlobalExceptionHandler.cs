e459d2b805baa06fab518311c9592374e3a13142
ervinhun <127263111+ervinhun@users.noreply.github.com>
2025-09-23 18:28:39 +0200

    Program.cs modification

---
+++ server/Api/MyGlobalExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api;
+
+public class MyGlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message
+        };
+        await httpContext.Response.WriteAsJsonAsync(problemDetails);
+        return true;
+    }
+}
