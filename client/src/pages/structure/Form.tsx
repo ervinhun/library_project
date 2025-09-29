6dda8777ca3121100edc9e1d35b363d160f4fefb
ervinhun <127263111+ervinhun@users.noreply.github.com>
2025-09-30 00:43:34 +0200

    Visual update

--- client/src/pages/structure/Form.tsx
+++ client/src/pages/structure/Form.tsx
@@ -36,6 +36,7 @@ export default function Form({
     const [title, setTitle] = useState("");
     const [pages, setPages] = useState<number>(0);
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
 
 
     const editing = !!publicId;
@@ -95,131 +96,146 @@ export default function Form({
 
     async function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
-        switch (formType) {
-            case "author": {
-                if (!editing) {
-                    const dto: AuthorResponseDto = await client.addAuthor(selectedAuthors?.[0].name ?? "");
-                    const newAuthor: Author = {
-                        id: dto.id!,
-                        name: dto.name ?? "",
-                        createdat: dto.createdat ?? new Date().toISOString()
-                    };
-                    setAuthors(prev => [...prev, newAuthor]);
-                } else {
-                    const dto: AuthorRequestDto = {id: selectedAuthors?.[0].id, name: selectedAuthors?.[0].name ?? ""};
-                    await client.updateAuthor(dto);
+        setLoading(true);
+        try {
+            switch (formType) {
+                case "author": {
+                    if (!editing) {
+                        const dto: AuthorResponseDto = await client.addAuthor(selectedAuthors?.[0].name ?? "");
+                        const newAuthor: Author = {
+                            id: dto.id!,
+                            name: dto.name ?? "",
+                            createdat: dto.createdat ?? new Date().toISOString()
+                        };
+                        setAuthors(prev => [...prev, newAuthor]);
+                    } else {
+                        const dto: AuthorRequestDto = {
+                            id: selectedAuthors?.[0].id,
+                            name: selectedAuthors?.[0].name ?? ""
+                        };
+                        await client.updateAuthor(dto);
 
-                    const updatedAuthor: Author = {
-                        id: dto.id ?? "",
-                        name: dto.name ?? "",
-                        createdat: new Date().toUTCString() ?? new Date().toISOString()
-                    };
+                        const updatedAuthor: Author = {
+                            id: dto.id ?? "",
+                            name: dto.name ?? "",
+                            createdat: new Date().toUTCString() ?? new Date().toISOString()
+                        };
 
-                    setAuthors(prev => prev.map(a => a.id === updatedAuthor.id ? updatedAuthor : a));
+                        setAuthors(prev => prev.map(a => a.id === updatedAuthor.id ? updatedAuthor : a));
 
-                }
-                break;
-            }
-
-            case "genre": {
-                if (!editing) {
-                    const newGenreDto = await client.addGenre(selectedGenre?.name ?? "");
-                    const newGenre: Genre = {
-                        id: newGenreDto.id!,
-                        name: newGenreDto.name ?? "",
-                        createdAt: newGenreDto.createdat ?? new Date().toISOString()
-                    };
-                    setGenres(prev => [...prev, newGenre]);
-                } else {
-                    const dto: GenreRequestDto = {id: selectedGenre?.id, name: selectedGenre?.name ?? ""};
-                    await client.updateGenre(dto);
-                    const updatedGenre: Genre = {
-                        id: dto.id!,
-                        name: dto.name ?? "",
-                        createdAt: new Date().toUTCString() ?? new Date().toISOString()
-                    };
-
-                    setGenres(prev => prev.map(g => g.id === updatedGenre.id ? updatedGenre : g));
-                }
-                break;
-            }
-
-            case "book": {
-                if (!title || pages <= 0 || !selectedGenre) {
-                    alert("Please fill out all the necessary fields - title, pages, genre.");
-                    return;
-                }
-                if (!editing) {
-
-                    const bookDto = {
-                        title,
-                        pages,
-                        genreid: selectedGenre.id,
-                    };
-
-                    const newBookResponse = await client.addBook(bookDto)
-                    const bookToAdd: Book = {
-                        id: newBookResponse.id!,
-                        title: newBookResponse.title ?? "",
-                        pages: newBookResponse.pages ?? 0,
-                        genreid: newBookResponse.genreid ?? "",
-                        genre: genres.find(g => g.id === newBookResponse.genreid)
-                            ?? {id: "", name: "Unknown", createdAt: new Date().toISOString()},
-                        authors: [],
-                        createdat: newBookResponse.createdat ?? new Date().toISOString()
                     }
-                    setBooks((prev) => [...prev, bookToAdd]);
+                    break;
+                }
 
-                    if (selectedAuthors.length > 0) {
-                        selectedAuthors.forEach(a => bookToAdd.authors.push(a));
-                        await client.updateBook(bookToAdd);
+                case "genre": {
+                    if (!editing) {
+                        const newGenreDto = await client.addGenre(selectedGenre?.name ?? "");
+                        const newGenre: Genre = {
+                            id: newGenreDto.id!,
+                            name: newGenreDto.name ?? "",
+                            createdAt: newGenreDto.createdat ?? new Date().toISOString()
+                        };
+                        setGenres(prev => [...prev, newGenre]);
+                    } else {
+                        const dto: GenreRequestDto = {id: selectedGenre?.id, name: selectedGenre?.name ?? ""};
+                        await client.updateGenre(dto);
+                        const updatedGenre: Genre = {
+                            id: dto.id!,
+                            name: dto.name ?? "",
+                            createdAt: new Date().toUTCString() ?? new Date().toISOString()
+                        };
+
+                        setGenres(prev => prev.map(g => g.id === updatedGenre.id ? updatedGenre : g));
                     }
-                } else {
-                    if (!publicId) {
-                        alert("Missing book ID for editing.");
+                    break;
+                }
+
+                case "book": {
+                    if (!title || pages <= 0 || !selectedGenre) {
+                        alert("Please fill out all the necessary fields - title, pages, genre.");
                         return;
                     }
-                    const genreDto: GenreResponseDto = {
-                        id: selectedGenre.id,
-                        name: selectedGenre.name,
-                        createdat: selectedGenre.createdAt
+                    if (!editing) {
+
+                        const bookDto = {
+                            title,
+                            pages,
+                            genreid: selectedGenre.id,
+                        };
+
+                        const newBookResponse = await client.addBook(bookDto)
+                        const bookToAdd: Book = {
+                            id: newBookResponse.id!,
+                            title: newBookResponse.title ?? "",
+                            pages: newBookResponse.pages ?? 0,
+                            genreid: newBookResponse.genreid ?? "",
+                            genre: genres.find(g => g.id === newBookResponse.genreid)
+                                ?? {id: "", name: "Unknown", createdAt: new Date().toISOString()},
+                            authors: [],
+                            createdat: newBookResponse.createdat ?? new Date().toISOString()
+                        }
+                        setBooks((prev) => [...prev, bookToAdd]);
+
+                        if (selectedAuthors.length > 0) {
+                            selectedAuthors.forEach(a => bookToAdd.authors.push(a));
+                            await client.updateBook(bookToAdd);
+                        }
+                    } else {
+                        if (!publicId) {
+                            alert("Missing book ID for editing.");
+                            return;
+                        }
+                        const genreDto: GenreResponseDto = {
+                            id: selectedGenre.id,
+                            name: selectedGenre.name,
+                            createdat: selectedGenre.createdAt
+                        }
+                        const authorsDto: AuthorResponseDto[] = selectedAuthors.map(a => ({
+                            id: a.id,
+                            name: a.name,
+                            createdat: a.createdat
+                        }));
+
+                        const dto = {
+                            id: selectedBook?.id ?? publicId,
+                            title,
+                            pages,
+                            genreid: selectedGenre.id,
+                            genre: genreDto ?? undefined,
+                            authors: authorsDto ?? undefined
+                        };
+                        await client.updateBook(dto);
+                        const updatedBook: Book = {
+                            id: dto.id ?? "",
+                            title: dto.title ?? "",
+                            pages: dto.pages ?? 0,
+                            genreid: dto.genreid ?? "",
+                            genre: selectedGenre,
+                            authors: selectedAuthors,
+                            createdat: new Date().toUTCString() ?? new Date().toISOString()
+                        };
+
+                        setBooks(prev => prev.map(b => b.id === updatedBook.id ? updatedBook : b));
                     }
-                    const authorsDto: AuthorResponseDto[] = selectedAuthors.map(a => ({
-                        id: a.id,
-                        name: a.name,
-                        createdat: a.createdat
-                    }));
-
-                    const dto = {
-                        id: selectedBook?.id ?? publicId,
-                        title,
-                        pages,
-                        genreid: selectedGenre.id,
-                        genre: genreDto ?? undefined,
-                        authors: authorsDto ?? undefined
-                    };
-                    await client.updateBook(dto);
-                    const updatedBook: Book = {
-                        id: dto.id ?? "",
-                        title: dto.title ?? "",
-                        pages: dto.pages ?? 0,
-                        genreid: dto.genreid ?? "",
-                        genre: selectedGenre,
-                        authors: selectedAuthors,
-                        createdat: new Date().toUTCString() ?? new Date().toISOString()
-                    };
-
-                    setBooks(prev => prev.map(b => b.id === updatedBook.id ? updatedBook : b));
                 }
             }
+            onClose();
+
+        } catch (e) {
+            alert(`Error: ${(e as Error).message}`);
+        } finally {
+            setLoading(false);
         }
-        onClose();
     }
 
     if (!open) return null;
 
     return (
         <>
+            <button className="btn btn-primary" type="submit" disabled={loading}>
+                {loading ? <span className="loading loading-spinner loading-sm"></span> : "Save"}
+            </button>
+
             <div className="fixed inset-0 bg-opacity-50 z-40" onClick={onClose}/>
             <div
                 className="fixed top-0 right-0 h-full w-full md:w-1/3 bg-base-100 z-50 shadow-lg overflow-auto rounded-box p-2 border border-b-primary-content"
@@ -337,8 +353,11 @@ export default function Form({
                         </>
                     )}
 
-                    <button className="btn btn-success" type="submit">
-                        Save
+                    <button className="btn btn-success" type="submit" disabled={loading}>
+                        {loading
+                            ? <span className="loading loading-spinner loading-sm"></span>
+                            : "Save"
+                        }
                     </button>
                 </form>
             </div>
